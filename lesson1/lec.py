3021bd4f83921a2ac9e7159aede4a6dad8159dfe
Lillyna <96077239+Lillyna@users.noreply.github.com>
2022-09-04 00:00:07 +0300

    Initial commit

---
+++ lesson1/lec.py
@@ -0,0 +1,25 @@
+# типы данных int float boolean str list None
+a = 1243
+value = None
+print(type(value))
+print(type(a))
+str = 'qwerty'
+print(str) #вывод строки
+
+s = "hello 'world'"
+ss = 'hello "world"'
+sss = 'hello \'wolrd\'' #эскейп-последовательности
+print(sss)
+
+print(s,'-',ss,'-',sss)
+print('{} - {} - {}'.format(s,ss,sss))
+print('{1} - {2} - {0}'.format(s,ss,sss))
+print (f'{s} - {ss} - {sss}')#интерполяция
+
+boo = True
+print(boo)
+
+list = []
+list1 = [1,2,3,4,5,'hello']
+col = [1,2,3]
+print(list, list1, col)
